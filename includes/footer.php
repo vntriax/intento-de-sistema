30382482a74328cff031dcd6a2a8d5c048c443a7
vntriax <vntriax@gmail.com>
2026-03-19 14:02:27 -0400

    Add footer.php with copyright and entity name

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<?php
+// includes/footer.php
+?>
+<footer class="footer">
+    <p>&copy; <?php echo date('Y'); ?> <?php echo $config['nombre_ente'] ?? 'Sistema de Prestaciones'; ?>. 
+    Basado en LOTTT Artículo 142 [[51]] y tasas BCV [[59]].</p>
+</footer>
